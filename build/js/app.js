423f42854335b18dfe007e58fe8abeb71ae6899b
Mario Godinez Medina <futureweb2000@gmail.com>
2015-11-16 22:38:43 -0600

    agragada la rutas y borrados los archivos porque hicimos directivaas , ademas arreglamos el problema de la peticion http el problema era que estaba mal el json

--- build/js/app.js
+++ build/js/app.js
@@ -1,52 +1,117 @@
 (function(){
-	var app = angular.module('vivero', []);
+	var app = angular.module('vivero', ['ngRoute']);
+	app.config(['$routeProvider', function($routeProvider){
+			$routeProvider
+				.when('/', {
+
+					templateUrl: 'views/pagina-principal.html'
+					// controller: 'csdcsdController'
+					// controllerAs: 'asasCtrl'
+				})
+				.when('/plantas',{
+
+					templateUrl: 'views/plantas.html'
+					// controller: 'PlantaController'
+				})
+				.when('/nosotros',{
+
+					templateUrl: 'views/nosotros.html'
+					// controller: 'TabsController'
+					// controllerAs: 'asasCtrl'
+				})
+
+				.when('/contacto',{
+
+					templateUrl: 'views/contacto.html'
+					// controller: 'csdcsdController'
+					// controllerAs: 'asasCtrl'
+				})
+
+		}]);
 
 
-	app.controller('PlantaController', function(){
-		
-		this.planta = {
+	
 
-			nombre:"ficus",
-			precio: 150,
-			categoria:"palmas",
-			exposicion: "sol",
-			id: 001
-		}
+	app.controller('PlantaController',['$scope', '$http',function($scope,$http){
 
-		this.categorias = [
-		"palmas",
-		"colgantess",
-		"rosales", 
-		"arbustosss", 
-		"piedras", 
-		"pinos"]
+			$scope.plantas = [];
+			$http.get('js/plantas.json')
+				.success(function(datos){
+					$scope.plantas = datos;
 
-	});
+				});
 
 
+	}]);
+
+
+	// app.controller('TabsController', function(){
+
+	// 	this.tab = 1;
+
+	// 	this.selectTab = function(tab){
+
+	// 		this.tab = tab;
+	// 	}
+	// });
+
+	// 		//Directivas//
+
+	// app.directive('paginaPrincipal', function(){
+	// 	return{
+	// 		restrict: 'E',
+	// 		templateUrl: 'views/pagina-principal.html'
+	// 	}
+
+	// });
+
+
+	// app.directive('paginaContacto', function(){
+	// 	return{
+	// 		restrict: 'E',
+	// 		templateUrl: 'views/contacto.html'
+	// 	}
+
+	// });
+
+
+	// app.directive('paginaNosotros', function(){
+	// 	return{
+	// 		restrict: 'E',
+	// 		templateUrl: 'views/nosotros.html'
+	// 	}
+
+	// });
+
+	// app.directive('paginaPlantas', function(){
+	// 	return{
+	// 		restrict: 'E',
+	// 		templateUrl: 'views/plantas.html'
+	// 	}
+
+	// });
+
+
+	
 
 
 
+	// $('.showNav').on('click', function(){
+
+	// 	$('.nav ul').slideToggle(300,'linear');
+	// });
+
+	// loadGoogleIcons('https://fonts.googleapis.com/icon?family=Material+Icons');
 
 
+	// //Cargar CSS desde JS para EVITE ser Bloqueantes
 
-
-	$('.showNav').on('click', function(){
-
-		$('.nav ul').slideToggle(300,'linear');
-	});
-
-	loadGoogleIcons('https://fonts.googleapis.com/icon?family=Material+Icons');
-
-
-	//Cargar CSS desde JS para EVITE ser Bloqueantes
-
-	function loadGoogleIcons(url){
-		var elem  = document.createElement('link');
-		elem.rel = 'stylesheet';
-		elem.href = url;
-		document.head.appendChild(elem);
-	}
+	// function loadGoogleIcons(url){
+	// 	var elem  = document.createElement('link');
+	// 	elem.rel = 'stylesheet';
+	// 	elem.href = url;
+	// 	document.head.appendChild(elem);
+	// }
 
 
 
